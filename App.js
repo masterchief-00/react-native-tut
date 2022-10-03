d3011210fa8c4d15c24ffbb291d1f24df22cd33c
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2022-10-03 21:12:57 +0200

    lesson 5

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Shaun");
+  const [age, setAge] = useState(30);
+
+  const clickHandler = () => {
+    setPerson({
+      name: "luigi",
+      age: 45,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter Name:</Text>
+      <TextInput
+        placeholder="e.g. John Doe"
+        onChangeText={(val) => setName(val)}
+        style={styles.input}
+      />
+      <TextInput
+      keyboardType="numeric"
+        placeholder="e.g. 99"
+        onChangeText={(val) => setAge(val)}
+        style={styles.input}
+      />
+      <Text>
+        Name: {name}, age: {age}
+      </Text>
     </View>
   );
 }
@@ -13,8 +36,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    padding: 8,
+    borderColor: "grey",
+    borderRadius: 5,
+    margin: 10,
+    width: 200,
   },
 });
